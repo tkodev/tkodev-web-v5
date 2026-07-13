ab0dc37f0a0c074c135e07d7f698e8b40868dcb7
Tony Ko <tony@tko.dev>
2026-07-13 02:37:54 -0400

    refactor(theme): remove the film grain feature

--- src/components/templates/overlay.tsx
+++ src/components/templates/overlay.tsx
@@ -1,10 +1,9 @@
 import { forwardRef, type HTMLAttributes } from 'react'
 import { Cursor } from '@/components/atoms/cursor'
-import { Grain } from '@/components/atoms/grain'
 import { cn, cva, type VariantProps } from '@/utils/theme'
 
 const styles = {
-  root: cva('')
+  root: cva('pointer-events-none fixed inset-0')
 }
 
 type OverlayRef = HTMLDivElement
@@ -15,7 +14,6 @@ const Overlay = forwardRef<OverlayRef, OverlayProps>((props, ref) => {
 
   return (
     <div ref={ref} className={cn(styles.root({ className }))} {...rest}>
-      <Grain />
       <Cursor />
     </div>
   )
